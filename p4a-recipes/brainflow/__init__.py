60873db4f66188a71ed639f1f77c65e088e38d59
thomas <windwerfer@gmail.com>
2023-04-18 14:25:37 +0700

    buildozer_recipe_7

--- p4a-recipes/brainflow/__init__.py
+++ p4a-recipes/brainflow/__init__.py
@@ -2,9 +2,12 @@ from pythonforandroid.recipe import PythonRecipe
 
 class BrainflowRecipe(PythonRecipe):
     version = '9.9.9'
-    url = 'https://github.com/windwerfer/compile_kivy_with_github_sample/raw/main/p4a-recipes/brainflow/brainflow-{version}.tar.gz'
+    url = 'https://raw.githubusercontent.com/windwerfer/compile_kivy_and_brainflow_github_for_arm64_sample/main/p4a-recipes/brainflow/brainflow-{version}.tar.gz'
     name = 'brainflow'
     depends = ['nptyping', 'numpy']
     md5sum = '53e1ca801ca5a8641a6cc21c7ca48e26'
 
 recipe = BrainflowRecipe()
+
+# test
+# wget https://raw.githubusercontent.com/windwerfer/compile_kivy_and_brainflow_github_for_arm64_sample/main/p4a-recipes/brainflow/brainflow-9.9.9.tar.gz
