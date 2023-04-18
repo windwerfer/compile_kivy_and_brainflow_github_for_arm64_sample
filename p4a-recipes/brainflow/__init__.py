c1a3a269ed3d92e88e8295f5d4683b906df134fb
thomas <windwerfer@gmail.com>
2023-04-18 14:00:18 +0700

    buildozer_recipies_4

--- p4a-recipes/brainflow/__init__.py
+++ p4a-recipes/brainflow/__init__.py
@@ -5,5 +5,6 @@ class BrainflowRecipe(PythonRecipe):
     url = 'https://github.com/windwerfer/compile_kivy_with_github_sample/raw/main/p4a-recipes/brainflow/brainflow-{version}.tar.gz'
     name = 'brainflow'
     depends = ['nptyping', 'numpy']
+    md5sum = '53e1ca801ca5a8641a6cc21c7ca48e26'
 
 recipe = BrainflowRecipe()
