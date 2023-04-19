b7607ee8c374edbe68812780803da098df70e915
thomas <windwerfer@gmail.com>
2023-04-20 01:49:27 +0700

    aber nu

--- main.py
+++ main.py
@@ -7,15 +7,29 @@ import kivy.uix.button
 from kivy.app import App
 from kivy.uix.button import Button
 
-import pkg_resources
+from importlib.metadata import packages_distributions
+from importlib.metadata import version
 
 class SimpleApp(kivy.app.App):
     def build(self):
-        installed_packages = pkg_resources.working_set
-        installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-             for i in installed_packages])
+        installed_packages = list( packages_distributions().keys() )
         # print(installed_packages_list)
-        merged_string = '\n'.join([' '.join(pair) for pair in installed_packages_list])
+        installed_packages.sort()
+        
+        li = []
+        if True: breakpoint()
+        for pa in installed_packages:
+            pa0 = pa
+            ver = 'xxx'
+            try: 
+                ver = version(pa0) 
+            except:
+                pass
+        
+
+            li.append(f"{pa0} == {ver}")
+
+        merged_string = '\n'.join(li)
         self.textInput = kivy.uix.textinput.TextInput(text=merged_string)
 
         # Wrap the TextInput object in a ScrollView
