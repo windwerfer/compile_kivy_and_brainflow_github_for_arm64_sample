c72b6b10818db3625e6901a38358dc8d0b82c2f5
thomas <windwerfer@gmail.com>
2023-04-19 12:44:26 +0700

    using wheel

--- p4a-recipes/brainflow/__init__.py
+++ p4a-recipes/brainflow/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class BrainflowRecipe(PythonRecipe):
     version = '9.9.9'
-    url = 'https://raw.githubusercontent.com/windwerfer/compile_kivy_and_brainflow_github_for_arm64_sample/main/p4a-recipes/brainflow/brainflow-{version}.tar.gz'
+    url = 'https://raw.githubusercontent.com/windwerfer/compile_kivy_and_brainflow_github_for_arm64_sample/main/p4a-recipes/brainflow/brainflow-{version}-py3-none-any.whl'
     name = 'brainflow'
     depends = ['nptyping', 'numpy']
     md5sum = '53e1ca801ca5a8641a6cc21c7ca48e26'
