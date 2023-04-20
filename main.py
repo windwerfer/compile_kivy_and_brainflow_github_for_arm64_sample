24758f06cf6113dc2f7fb81edb0597f1ff1f4df8
thomas <windwerfer@gmail.com>
2023-04-20 10:56:15 +0700

    back to package resoirces..

--- main.py
+++ main.py
@@ -7,29 +7,43 @@ import kivy.uix.button
 from kivy.app import App
 from kivy.uix.button import Button
 
-from importlib.metadata import packages_distributions
-from importlib.metadata import version
+import sys
+import pkg_resources
+
+# if sys.version_info >= (3, 8):
+#     from importlib import metadata as importlib_metadata
+# else:
+#     import importlib_metadata
+# from pip._internal.utils.misc import get_installed_distributions
+
+
+# from importlib.metadata import packages_distributions
+# from importlib.metadata import version
 
 class SimpleApp(kivy.app.App):
     def build(self):
-        installed_packages = list( packages_distributions().keys() )
-        # print(installed_packages_list)
-        installed_packages.sort()
+        # installed_packages = list( packages_distributions().keys() )
+        # # print(installed_packages_list)
+        # installed_packages.sort()
         
+        # if True: breakpoint()
+        # for pa in installed_packages:
+        #     pa0 = pa
+        #     ver = 'xxx'
+        #     try: 
+        #         ver = version(pa0) 
+        #     except:
+        #         pass
+
         li = []
-        if True: breakpoint()
-        for pa in installed_packages:
-            pa0 = pa
-            ver = 'xxx'
-            try: 
-                ver = version(pa0) 
-            except:
-                pass
-        
+        # Get a list of all installed packages
+        # packages = pkg_resources.working_set
+        packages = sorted(pkg_resources.working_set, key=lambda p: p.project_name.lower())
 
-            li.append(f"{pa0} == {ver}")
-
-        merged_string = '\n'.join(li)
+        for package in packages:
+            li.append(f"{package.project_name} == {package.version}")
+            # print(package.key, package.version)
+        merged_string = 'python version ' + str(sys.version_info)+ '\n' + '\n'.join(li)
         self.textInput = kivy.uix.textinput.TextInput(text=merged_string)
 
         # Wrap the TextInput object in a ScrollView
